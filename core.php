3d1cb40250b6b32685601663a03a192ef33096e8
Dallas Read <dallas@excitecreative.ca>
2015-01-22 13:55:00 -0400

    Handle including public JS appropriately.

--- core.php
+++ core.php
@@ -1,9 +1,16 @@
 <?php
-include("widget.php");
-add_action('wp_head', 'emailopened_public_tag');
-function emailopened_public_tag() {
-	echo '<script type="text/javascript" src="'.EO_FORMS_URL.'assets/js/cp.js"></script>';
-}
 
+	include("widget.php");
+
+	// add_action('wp_head', 'emailopened_public_tag');
+	add_action( 'wp_enqueue_scripts', 'eo_public_scripts' );
+
+	// function emailopened_public_tag() {
+	// 	echo '<script type="text/javascript" src="'.EO_FORMS_URL.'assets/js/cp.js"></script>';
+	// }
+	
+	function eo_public_scripts() {
+		wp_enqueue_script('eo-forms', 'https://' . EO_SUBDOMAIN . '.emailopened.com/js/EO-forms.js', array( 'jquery' ));
+	}
 
 ?>
