fb59c0d8f2d8e0291ce72c617fd3132cfac9d8a8
Dallas Read <dallas@excitecreative.ca>
2015-01-22 17:12:47 -0400

    Alignments fix.

--- widget.php
+++ widget.php
@@ -20,45 +20,34 @@ class EmailOpened_Widget extends WP_Widget {
 
 	function widget( $args, $instance ) {
 		extract( $args, EXTR_SKIP );
-		echo $before_widget;
-		$title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
-		if ( !empty( $title ) ) { echo $before_title . $title . $after_title; }
-		if ( !empty( $instance['desc'] ) ) { echo "<p class=\"eo_description\">" . $instance['desc'] . "</p>"; }
 		$current_eoform = $instance['webform'];
 		
 		if ( ! empty( $current_eoform ) )
 		{
+			echo $before_widget;
+			$title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+			
+			if ( !empty( $title ) ) { echo $before_title . $title . $after_title; }
+			if ( !empty( $instance['desc'] ) ) { echo "<p class=\"eo_description\">" . $instance['desc'] . "</p>"; }
+			
+			$align = $instance['align'];
 			$current_forms = get_option( 'eoforms' );
 			
 			foreach($current_forms as $eoform)
 			{
 				if ($eoform['id'] == $current_eoform)
-				{
-					//Captcha
-					// if ( !empty( $instance['captcha'] ) && $instance['captcha'] == "checked" ) {
-					// 	preg_match('/<input type="submit" value="(.*)">/', $eoform["embed"], $button);
-					// 	$eoform["embed"] = preg_replace('/<input type="submit" value="(.*)">/', '<div id="cap_text" style="padding: 5px;"><label for="button_replace">Human Check *</label><script type="text/javascript">DrawBotBoot()</script></div><div id="button_replace" style="padding: 5px;"><button type="button" onClick="javascript:ValidBotBoot(\''.$button[1].'\', this)">'.$instance['vali'].'</button></div>', $eoform["embed"]);
-					// }
-					
+				{					
 					$eoform["embed"] = str_replace('type="submit"', 'class="btn btn-primary" type="submit"', $eoform["embed"]);
 					$eoform["embed"] = str_replace('type="email"', 'type="text"', $eoform["embed"]);
-					$eoform["embed"] = str_replace("<form ", '<form class="eo-embedded-subscribe-form widget-content" ', $eoform["embed"]);
+					$eoform["embed"] = str_replace("<form ", "<form class=\"eo-embedded-subscribe-form widget-content eo-align-$align\" ", $eoform["embed"]);
 					$eoform["embed"] = str_replace('</form>', '<div class="eo_response"></div></form>', $eoform["embed"]);
-					
-					$alignment = "";
-					$style = "";
-					if ( !empty( $instance['align'] ) ) { $alignment = 'text-align: '.$instance['align'].';'; }
-					if ( !empty( $instance['style'] ) ) { $style = 'class = "'.$instance['style'].'"'; }
 
 					echo $eoform["embed"];
 				}
-					
-				
 			}
+			
+			echo $after_widget;
 		}
-		else
-			echo "EmailOpened form is missing.";
-		echo $after_widget;
 	}
 
 
@@ -90,7 +79,6 @@ class EmailOpened_Widget extends WP_Widget {
 		<label for="<?php echo $this->get_field_id( 'desc' ); ?>"><?php _e( 'Description:' ); ?></label> 
 		<textarea class="widefat" id="<?php echo $this->get_field_id( 'desc' ); ?>" height="4" name="<?php echo $this->get_field_name( 'desc' ); ?>"><?php echo $instance[ 'desc' ] ?></textarea>
 		</p>
-		<hr style="width: 75%; align: center;">
 		<?php
 		$current_forms = get_option( 'eoforms' );
 		
@@ -111,33 +99,23 @@ class EmailOpened_Widget extends WP_Widget {
 			echo '<p class="center" style="margin-top: 20px; color: red; border: 1px solid red;">	
 				<b>You currently have no Forms.<br />Please check your API Key and build forms at <a href="' . EO_URL . '/forms/new" target="_blank">' . EO_URL . '/forms</a>!</b>
 			</p>'; 
-		
-		echo '<p><label for="'.$this->get_field_name( 'align' ).'">'._e( 'Form Alignment:' ).'</label>&nbsp;&nbsp;<input type="radio" name='.$this->get_field_name( 'align' ).' value="left"';
-		if ($instance[ 'align' ] == "left")
-			echo " checked ";
-		echo '>Left <input type="radio" name='.$this->get_field_name( 'align' ).' value="center"';
-		if ($instance[ 'align' ] == "center")
-			echo " checked ";
-		echo '>Center <input type="radio" name='.$this->get_field_name( 'align' ).' value="right"';
-		if ($instance[ 'align' ] == "right")
-			echo " checked ";
-		echo '>Right</p>';
-		
-		echo '<hr style="width: 75%; align: center;">';
-		
-		echo '<p><input type="checkbox" id="'.$this->get_field_id( 'captcha' ).'" name="'.$this->get_field_name( 'captcha' ).'" value="checked" '.$instance[ 'captcha' ].' >Use Captcha</p>';
-		
-		echo '<p><label for="'.$this->get_field_name( 'vali' ).'">'._e( 'Captcha "Validate" button text:' ).'</label><input id="'.$this->get_field_id( 'vali' ).'" name="'.$this->get_field_name( 'vali' ).'" type="text" value="'.$instance[ 'vali' ].'" /></p>';
-		
-		echo '<hr style="width: 75%; align: center;">';
 			
-		echo '<p"><label for="'.$this->get_field_name( 'redirect' ).'">'._e( 'Where to Redirect:' ).'</label>&nbsp;&nbsp;<input type="radio" name='.$this->get_field_name( 'redirect' ).' value="_blank"';
-		if ($instance[ 'redirect' ] == "_blank")
-			echo " checked ";
-		echo '>_blank <input type="radio" name='.$this->get_field_name( 'redirect' ).' value="_self"';
-		if ($instance[ 'redirect' ] == "_self")
-			echo " checked ";
-		echo '>_self</p>';
+		
+		echo '<p><label for="'.$this->get_field_name( 'align' ).'">'._e( 'Form Alignment:' ).'</label>';
+			
+		$alignments = array('Left', 'Center', 'Right', 'None');
+		$align_field_name = $this->get_field_name( 'align' );
+		
+		foreach ($alignments as $alignment) {
+			$checked = $instance['align'] == strtolower($alignment) ? " checked=\"checked\"" : "";
+			echo "<input type=\"radio\" name=\"" . $align_field_name . "\" value=\"" . strtolower($alignment) . "\"$checked> $alignment &nbsp; ";
+		}
+		
+		echo "</p>";
+		
+		echo '<p>
+			<input type="checkbox" id="'.$this->get_field_id( 'captcha' ).'" name="'.$this->get_field_name( 'captcha' ).'" value="checked" '.$instance[ 'captcha' ].' >Use Captcha
+		</p>';
 		
 		echo '<p><label for="'.$this->get_field_id( 'style' ).'">'._e( 'Custom style Class:' ).'</label>&nbsp;&nbsp;<input id="'.$this->get_field_id( 'style' ).'" name="'.$this->get_field_name( 'style' ).'" type="text" value="'.$instance[ 'style' ].'" /></p>';
 		
